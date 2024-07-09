e18ce23d52e3ec62768cb7f19de8363f66d4787c
Manoj Khati <mksea18@gmail.com>
2024-07-09 21:55:03 +0545

    pdf modules updated

--- modules/pdf/pdf.controller.js
+++ modules/pdf/pdf.controller.js
@@ -1,9 +1,16 @@
 const { pdfDoc } = require("../../utils/pdf");
 const createPDF = async (payload) => {
   const { image, text } = payload;
-  const isvalidimg = image.includes("image.png");
-  if (!isvalidimg) throw new Error("Invalid Image");
-  const result = await pdfDoc(image);
-  return result;
+
+  const isvalidimg = typeof image === "string" && image.includes("image.png");
+  if (!isvalidimg) {
+    throw new Error("Invalid Image");
+  }
+  try {
+    const result = await pdfDoc(image, text);
+    return result;
+  } catch (e) {
+    throw new Error(`Failed to create PDF: ${error.message}`);
+  }
 };
 module.exports = { createPDF };

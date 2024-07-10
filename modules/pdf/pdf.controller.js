8d9507bda645aaa371715e50491576f38290acd0
Manoj Khati <mksea18@gmail.com>
2024-07-10 12:23:51 +0545

    updated

--- modules/pdf/pdf.controller.js
+++ modules/pdf/pdf.controller.js
@@ -1,16 +1,19 @@
 const { pdfDoc } = require("../../utils/pdf");
+
 const createPDF = async (payload) => {
-  const { image, text } = payload;
+  const { imagePath, text } = payload;
 
-  if (typeof image !== "string") {
-    throw new Error("Invalid image");
+  //Validate text input
+  if (typeof text !== "string" || text.trim() === "") {
+    throw new Error("Invalid text");
   }
-  const isvalidimg = image.includes("./image.png");
-  if (!isvalidimg) {
-    throw new Error("Invalid Image");
+  //validate imagePath in
+  if (typeof imagePath !== "string" || imagePath.trim() === "") {
+    throw new Error("Invalid image path");
   }
-
-  const result = await pdfDoc(image, text);
+  const isvalidimg = imagePath.includes("image.png");
+  const result = await createPDF(imagePath, text);
   return result;
 };
+
 module.exports = { createPDF };
--- utils/pdf.js
+++ utils/pdf.js
@@ -1,28 +1,31 @@
 const PDFDocument = require("pdfkit");
 const fs = require("fs");
 const { Module } = require("module");
+
+const pdfDoc = ({ text, imagePath }) => {
+  const pdfDoc = new PDFDocument();
+  pdfDoc.pipe(fs.createWriteStream("Document.pdf"));
+  pdfDoc.image("imagePath", {
+    fit: [250, 300],
+    align: "center",
+    valign: "center",
+  });
+  pdfDoc.end();
+};
 // Create a new PDF document
-const pdfDoc = new PDFDocument();
 
 // Pipe the PDF into a writable stream (create the PDF file)
-pdfDoc.pipe(fs.createWriteStream("Document.pdf"));
 
 // Add some text to the document
-pdfDoc.text("This is my!", 100, 100);
+// pdfDoc.text("This is my!", 100, 100);
 // pdfDoc.fillColor("red").fontSize(17).text("PDF", 305, 150);
 
 // Path to the image
 // const imagePath = "./node.jpeg";
 
 // Add an image
-pdfDoc.image("image.png", {
-  fit: [250, 300],
-  align: "center",
-  valign: "center",
-});
 
 // Finalize the PDF and end the stream
-pdfDoc.end();
 
 console.log("PDF generated successfully:");
 module.exports = pdfDoc;
